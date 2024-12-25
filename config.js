f6e3127ece9a10df916f8c3fbb3d341710132fec
Justin Elmore <jelmore1674@pm.me>
2024-12-25 13:37:14 -0600

    update to use better preset config

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  endpoint: 'https://forgejo.justinelmore.dev/api/v1', // replace it with your actual endpoint
-  gitAuthor: 'Renovate Bot <renovate-bot@forgejo.justinelmore.dev>',
-  platform: 'gitea',
   onboardingConfigFileName: 'renovate.json',
+  onboardingConfig: {
+    $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+    extends: ['config:best-practices'],
+  },
   autodiscover: true,
   optimizeForDisabled: true,
   prHourlyLimit: 30,
