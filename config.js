3a1b0782324819c201b3d7867e2e2cea465bcadd
Justin Elmore <jelmore1674@pm.me>
2024-12-25 14:02:18 -0600

    update endpoint

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  onboardingConfigFileName: 'renovate.json',
+  endpoint: 'https://forgejo.justinelmore.dev/api/v1', // replace it with your actual endpoint
   onboardingConfig: {
     $schema: 'https://docs.renovatebot.com/renovate-schema.json',
     extends: ['config:best-practices'],
   },
+  platform: 'gitea',
+  onboardingConfigFileName: 'renovate.json',
   autodiscover: true,
   optimizeForDisabled: true,
   prHourlyLimit: 30,
